e49911847859053380bb60ebef7f1a36e94c8724
captjay98 <captjay98@gmail.com>
2022-08-04 17:01:22 +0100

    added empty functions for ver 0.One

--- console.py
+++ console.py
@@ -24,6 +24,36 @@ class HBNBCommand(cmd.Cmd):
         """Does nothing"""
         pass
 
+    def do_create(self, arg):
+        """Creates new instance of BaseModel,
+        saves it to json and prints Id"""
+
+        pass
+
+    def do_show(self, arg):
+        """Prints string representation of an
+        instance based on class name and id"""
+
+        pass
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on classname and id"""
+
+        pass
+
+    def do_all(self, arg):
+        """prints all string representation of all instances
+        based or not on the class name"""
+
+        pass
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id
+        by adding or updating attribute
+        and save the change into the JSON file"""
+
+        pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
