5e19696ff5a81b1848f46fb935d41bae8223482a
captjay98linode <captjay98@gmail.com>
2022-08-06 19:22:11 +0000

    pycodestyle check

--- models/base_model.py
+++ models/base_model.py
@@ -27,7 +27,7 @@ class BaseModel:
 
     def __str__(self):
         """Returns string representation"""
-        return("[{}] ({}) {}".format(self.__class__.__name__,
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__))
 
     def save(self):
