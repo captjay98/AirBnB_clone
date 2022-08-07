571fa341d2bbfa56b26eaed33efe4e6e3836f79a
Adebimpe Islamiyyah Adefila <95176308+MiyyahCodes@users.noreply.github.com>
2022-08-07 16:36:50 +0100

    userrrr

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Creates a new User object"""
+    """This method creates a new User object."""
 
     email = ""
     password = ""
